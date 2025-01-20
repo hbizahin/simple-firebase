3905d7e5e8c145ff3fe718427c9f60b0b83fa8f4
hbizahin <hbizahin@gmail.com>
2025-01-21 02:45:28 +0600

    github authentication added

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,14 +3,15 @@ import { getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
 import { GoogleAuthProvider } from "firebase/auth";
 import { useState } from "react";
+import { GithubAuthProvider } from "firebase/auth";
 const Login = () => {
   const [user, setUser] = useState(null);
   const auth = getAuth(app);
-  console.log(app);
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then((result) => {
         const loggedInUser = result.user;
         console.log(loggedInUser);
@@ -20,6 +21,18 @@ const Login = () => {
         console.log("error", error.message);
       });
   };
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then(result => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        setUser(loggedUser);
+      })
+      .catch(error => {
+        console.log("error", error.message);
+      });
+  };
+
   const handleSignOut = () => {
     signOut(auth)
       .then((result) => {
@@ -32,17 +45,22 @@ const Login = () => {
   };
   return (
     <div>
-      {user ? 
-        <button onClick={handleSignOut}>Sign Out</button> : 
-        <button onClick={handleGoogleSignIn}>Google Login</button>
-      }
+      {user ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <div>
+          <button onClick={handleGoogleSignIn}>Google Login</button>
+          <button onClick={handleGithubSignIn}>Github Login</button>
+        </div>
+      )}
 
-      {user && <div>
+      {user && (
+        <div>
           <h3>User: {user.displayName}</h3>
           <p>Email: {user.email}</p>
           <img src={user.photoURL} alt="" />
         </div>
-      }
+      )}
     </div>
   );
 };
